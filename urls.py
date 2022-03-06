7b5c800dcb6939d22c408698336b6df9f3edf31a
Leonardo Bitto <leonardobitto@gmail.com>
2022-03-06 10:46:49 +0100

    fixed bug in urls.py, admin must be first

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
-from django.conf.urls.static import static
+#from django.conf.urls.static import static
 
 app_name = 'Acms'
 urlpatterns = [
     
 ]
 
-# add this lines
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # add this lines
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
